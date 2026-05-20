2c6e037d1179c4f4de5dc95892f0549316aa8e91
JimGuo <guozhan1722@hotmail.com>
2026-05-19 21:51:24 -0600

    added video example

---
+++ MyGithubPage/Models/MediaItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MyGithubPage.Models
+{
+    public class MediaItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+        public string Type  { get; set; } //video, image, audio
+    }
+}
